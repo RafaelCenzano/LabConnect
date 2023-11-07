3865d47aac14ded22ed32054cee3a2df34f2b244
Abid Talukder <atalukder30@stuy.edu>
2023-11-07 17:05:11 -0500

    created scraping function that takes in links and returns professor names and links to their profile

--- labconnect/main/scrape.py
+++ labconnect/main/scrape.py
@@ -1,6 +1,52 @@
 from bs4 import BeautifulSoup
 import requests
 
+resources = {
+    "Center for Biotechnology and Interdisciplinary Studies (CBIS)": {
+        "faculty": "https://biotech.rpi.edu/faculty",
+    },
+    "Center for Computational Innovations (CCI)":{
+        "faculty:" "https://cci.rpi.edu/faculty",
+    },
+    "Center for Materials, Devices, and Integrated Systems (CMDIS)": {
+        
+    },
+    "Experimental Media and Performing Arts Center (EMPAC)": {
+        
+    },
+    "Institute for Data Exploration and Applications (IDEA)": {
+        
+    },
+    "Institute for Energy, the Built Environment, and Smart Systems (EBESS)": {
+        
+    },
+    "Center for Architecture Science and Ecology (CASE)": {
+        
+    },
+    "Center for Automation Technologies and Systems (CATS)": {
+        
+    }, 
+    "Center for Future Energy Systems (CFES)": {
+        
+    }, 
+    "Center for Modeling, Simulation and Imaging in Medicine (CEMSIM)": {
+        
+    }, 
+    "Cognitive and Immersive Systems Lab (CISL)": {
+        
+    },
+    "Darrin Fresh Water Institute (DFWI)" : {
+        
+    }, 
+    "Network Science and Technology Center (NeST)": {
+        
+    }
+    
+    
+    
+    
+}
+
 def scrapeResearchCenters():
     url = "https://research.rpi.edu/research-centers"
     
@@ -42,6 +88,30 @@ def scrapeResearchCenters():
     
     return payload
 
+def getProfessors(url):
+    page = requests.get(url)
+    
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    links = soup.find_all('a')
+    professorLinks = []
+    
+    for link in links:
+        if (link.get('href') is not None and link.get('href').startswith('https://faculty.rpi.edu') and link.get_text().strip() != ""):
+            professorLinks.append(link)
+            
+    payload = []
+    
+    for link in professorLinks:
+        load = {}
+        load["link"] = link.get('href')
+        link = link.find_all('img')[0]
+        load["name"] = link.get('alt')
+        payload.append(load)
+        
+    print(payload)
+    
+
 
     
     # for i, a in enumerate(soup.find_all('a')):
@@ -54,4 +124,5 @@ def scrapeResearchCenters():
     #         )
                 
 
-print(scrapeResearchCenters())
+#print(scrapeResearchCenters())
+getProfessors("https://biotech.rpi.edu/faculty")
